e00ee02ff8e10b07f42ca1b0dac43d009debe315
weijiuzhu <12345678>
2019-06-30 15:06:20 +0800

    1.排序

---
+++ src/main/java/cn/com/crtvu/account/AccountVO.java
@@ -0,0 +1,14 @@
+package cn.com.crtvu.account;
+
+/**
+ * Created by weijiuzhu on 2019/6/10.
+ */
+public class AccountVO implements Cloneable{
+
+
+    public Object clone() throws CloneNotSupportedException {
+        return (AccountVO)super.clone();
+    }
+
+
+}
---
+++ src/main/java/cn/com/crtvu/account/test.java
@@ -0,0 +1,13 @@
+package cn.com.crtvu.account;
+
+/**
+ * Created by weijiuzhu on 2019/6/10.
+ */
+public class test {
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        AccountVO accountVO = new AccountVO();
+        AccountVO accountVO1 = (AccountVO) accountVO.clone();
+        System.out.println(accountVO == accountVO1);
+    }
+}
